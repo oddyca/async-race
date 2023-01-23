33f1fc1132d0cdf706b182c42d2b41ee35298b2e
Siarhei Prakapovich <prokser27@icloud.com>
2023-01-23 23:53:30 +0300

    feat: add winners table

--- src/components/winners/winners.ts
+++ src/components/winners/winners.ts
@@ -1,18 +1,80 @@
-import { getAllWinners, QueryParamStrings } from '../controller/controller';
+import { getAllCars, getAllWinners, QueryParamStrings } from '../controller/controller';
 
 export class Winners {
     constructor(){}
-
+    
     async render() {
+        const cars: QueryParamStrings = await getAllCars();
         const winners: QueryParamStrings = await getAllWinners();
         const winnersPage = document.createElement('div');
         winnersPage.classList.add('winners-container');
 
-        winners.map((car) => {
-            const carDiv = document.createElement('div');
-            carDiv.innerText = `${car.id}`
-            winnersPage.append(carDiv);
+        const winnersTable = document.createElement('div');
+        winnersTable.classList.add('winners-table');
+        const positionColumn = document.createElement('div'); // position column
+        positionColumn.classList.add('table-column');
+        const positionColumnTitle = document.createElement('div');
+        positionColumnTitle.innerText = 'Number';
+        positionColumnTitle.classList.add('column-title');
+        positionColumn.append(positionColumnTitle);
+        winnersTable.append(positionColumn);
+
+        const carImgColumn = document.createElement('div'); // car img column
+        carImgColumn.classList.add('table-column');
+        const carImgColumnTitle = document.createElement('div');
+        carImgColumnTitle.innerText = 'Car';
+        carImgColumnTitle.classList.add('column-title');
+        carImgColumn.append(carImgColumnTitle);
+        winnersTable.append(carImgColumn);
+
+        const carNameColumn = document.createElement('div'); // car name column
+        carNameColumn.classList.add('table-column');
+        const carNameColumnTitle = document.createElement('div');
+        carNameColumnTitle.innerText = 'Name';
+        carNameColumnTitle.classList.add('column-title');
+        carNameColumn.append(carNameColumnTitle);
+        winnersTable.append(carNameColumn);
+
+        const carWinsColumn = document.createElement('div'); // car wins column
+        carWinsColumn.classList.add('table-column');
+        const carWinsColumnTitle = document.createElement('div');
+        carWinsColumnTitle.innerText = 'Wins';
+        carWinsColumnTitle.classList.add('column-title');
+        carWinsColumn.append(carWinsColumnTitle);
+        winnersTable.append(carWinsColumn);
+
+        const carTimeColumn = document.createElement('div'); // car time column
+        carTimeColumn.classList.add('table-column');
+        const carTimeColumnTitle = document.createElement('div');
+        carTimeColumnTitle.innerText = 'Best Time';
+        carTimeColumnTitle.classList.add('column-title');
+        carTimeColumn.append(carTimeColumnTitle);
+        winnersTable.append(carTimeColumn);
+
+        winners.map((position) => {
+            const positionDiv = document.createElement('div');
+            positionDiv.innerText = position.id;
+            positionColumn.append(positionDiv);
+
+            const carIcon = document.createElement('div');
+            const carImg = document.createElement('img');
+            carIcon.setAttribute('alt', 'car image');
+            carIcon.append(carImg);
+            carImgColumn.append(carIcon);
+
+            const carName = document.createElement('div');
+            carName.innerText = '__';
+            carNameColumn.append(carName);
+
+            const carWins = document.createElement('div');
+            carWins.innerText = position.wins;
+            carWinsColumn.append(carWins);
+
+            const carTime = document.createElement('div');
+            carTime.innerText = position.time;
+            carTimeColumn.append(carTime);
         });
+        winnersPage.append(winnersTable)
         return winnersPage;
     }
 }
